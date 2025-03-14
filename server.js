0dacd5f80db616b253fd08e03a0f9c536d5e46bd
MahmoudEmadAbdullah <mahmoudemadcule2025@gmail.com>
2025-03-14 16:16:59 -0400

    Add helmet for HTTP header security and mongoSanitize to prevent MongoDB operator injection

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const fs = require('fs');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
 
 dotenv.config({path: './config.env'});
 const { connectRedis } = require('./src/config/redisConfig');
@@ -31,7 +33,6 @@ const app = express();
     await connectRedis();
 })();
 
-
 // Webhook
 app.post(
     '/webhook-checkout',
@@ -39,19 +40,19 @@ app.post(
     webhookCheckout
 );
 
-
 //Middlewares
 app.use(express.json({ limit: "100kb" }));
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(cookieParser());
 app.use(cors());
+app.use(mongoSanitize()); // to prevent MongoDB Operator Injection.
 
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan("dev"));
     console.log(`mode: ${process.env.NODE_ENV}`);
 };
-
 
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -64,22 +65,18 @@ app.get('/', (req, res) => {
     res.send("Welcome to My E-Commerce API!");
 });
 
-
 app.all('*', (req, res, next) => {
     next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 
-
 //Global error Handler Middleware for express
 app.use(globalError);
-
 
 //Server
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
     console.log(`Server Running on port ${port}...`);
 });
-
 
 //Handle rejection outside express
 process.on('unhandledRejection', (err) => {
